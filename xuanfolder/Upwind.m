1bb4ab7cc9b346d34a7c66f527045f522f628a9f
xuanP <pengxuan0101@gmail.com>
2017-04-13 11:20:08 +0200

    Update Upwind.m

--- xuanfolder/Upwind.m
+++ xuanfolder/Upwind.m
@@ -1,9 +1,9 @@
 %this .m file is for implementing article(12)
 function d = Upwind(u,sign)
-xf=Dx_for(u);
-yf=Dy_for(u);
-xb=Dx_back(u);
-yb=Dy_back(u);
+xf=dx_for(u);
+yf=dy_for(u);
+xb=dx_back(u);
+yb=dy_back(u);
 
 if sign<=0    %if k<=0 
     d=sqrt(max(xb,0)^2+min(xf,0)^2+max(yb,0)^2+min(yf,0)^2);
