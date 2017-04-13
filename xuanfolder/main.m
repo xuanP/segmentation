f18e456d37e013f25e7d5a9eccfd4e5e4efb0365
xuanP <pengxuan0101@gmail.com>
2017-04-13 12:53:29 +0200

    main.m
    This is the main program that you can run directly in matlab.
    Other files are just functions

---
+++ xuanfolder/main.m
@@ -0,0 +1,18 @@
+%this is the main program that you can run directly in matlab.
+%Other files are just functions
+clear
+close all;
+img=readImage('images.jpg');
+sigma=1.2; %parameter in Gaussien Kernel
+g=stopFunction(img,1,sigma);
+
+%initial LSF
+LSF=Ini_square(img,5);
+figure();
+imagesc(img);
+colormap(gray);
+hold on;
+[c,h]=contour(LSF,[0 0],'r');
+title('initial contour');
+%level set begins
+levelset=AOS(img, LSF, -0.3,0.7, 600 , g);
