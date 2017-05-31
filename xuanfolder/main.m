ee110e6a306340b18d495f9a1668ad7c0159b5a4
xuanP <pengxuan0101@gmail.com>
2017-05-31 23:24:28 +0200

    Update main.m
    Add some lines in order to modify all parameters more easily

--- xuanfolder/main.m
+++ xuanfolder/main.m
@@ -2,12 +2,17 @@
 %Other files are just functions
 clear
 close all;
-img=readImage('images.jpg');
-sigma=1.2; %parameter in Gaussien Kernel
-g=stopFunction(img,1,sigma);
-
+img=readImage('new.jpg');
+sigma=0.5; %parameter in Gaussien Kernel origin:1.2
+lambda=1;
+g=stopFunction(img,lambda,sigma);
+figure();
+imshow(g);
+title('edge indicator');
 %initial LSF
 LSF=Ini_square(img,5);
+%dphi=GetDifference(LSF);
+%test=dphi(:)./g(:);
 figure();
 %imagesc(img);=>to show a scalable image.
 %colormap(gray);
@@ -16,4 +21,4 @@ hold on;
 [c,h]=contour(LSF,[0 0],'r');
 title('initial contour');
 %level set begins
-levelset=AOS(img, LSF, -0.3,0.7, 600 , g);
+levelset=AOS(img, LSF, -0.3,5, 150 , g);
