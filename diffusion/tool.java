5823ffbf24c2b8df58d61400a665bb984625c9a6
mlorsini <marie-lou.orsini@insa-lyon.fr>
2017-06-01 15:26:17 +0200

    mise à jour des fonctions de diffusion et detecion de contours + ajout makefile

--- diffusion/tool.java
+++ diffusion/tool.java
@@ -4,8 +4,12 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.util.Arrays;
+import java.awt.Color;
 
 public class tool {
+	
+	
+	/*****Retourne le gradient d'une image (tableau de double) sous forme de tableau de double *****/
 	public static double[][] gradient(double[][] Source) throws IOException {
 		int w =  Source[0].length;
 		int h =  Source.length;
@@ -21,7 +25,9 @@ public class tool {
 		}
 		return Output;
 	}
-		
+	
+	
+	/*******Ouvre une image et la retourne en niveau de gris comme un tableau de double**********/
 	public static double[][] getArray(String path ) throws IOException{
 				
 			BufferedImage Source= ImageIO.read(new File(path));
@@ -29,9 +35,11 @@ public class tool {
 			int w =  Source.getWidth();
 			int h =  Source.getHeight();
 			double[][] Output = new double[h][w];
+			//double[][] Output2= new double[h+1][w+1];
 			
-			for(int y = 1; y < h; y++){
-				for(int x = 1; x < w; x++){
+			
+			for(int y = 0; y < h; y++){
+				for(int x = 0; x < w; x++){
 					int p = Source.getRGB(x,y);
 
 					int a = (p>>24)&0xff;
@@ -45,9 +53,12 @@ public class tool {
 				}
 			}
 		return Output;
+		
         }
         
 
+
+	/*****Enregistre l'image correspondante au tableau de double donné (valable pour les ".png")**********/
 	public static void getImage(double[][] Source ,String path) throws IOException{
 			int w =  Source[0].length;
 			int h =  Source.length;
@@ -59,8 +70,10 @@ public class tool {
 				for(int x = 0; x < h; x++){
 					int val=(int) Source[x][y];
 					int p =0;
+					Color myWhite = new Color(val, val, val);
+					int rgb = myWhite.getRGB();
 					p= (255<<24) | (val<<16) | (val<<8) | val;
-					Output.setRGB(y,x,p);
+					Output.setRGB(y,x,rgb);
 
 				}
 			}
